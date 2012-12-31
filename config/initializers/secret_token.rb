c05e765f2ccbf701304ecd15327034cfd05708e7
mattypreston <Matthew.Preston@Torex.com>
2012-12-31 17:58:19 +0000

    genesis commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MattPreston::Application.config.secret_token = '7ab31de06667560291c7a12a9f586aa157ef24fac76de1c8c536eb1ee751fc7bfcf461de70bba1390c50953b7aaaca268e97139cc28780c0c9f4e73e58237d31'
