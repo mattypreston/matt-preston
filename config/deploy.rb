e71b9ad1c6f345221f303946971fa8a1ee80cc49
mattypreston <Matthew.Preston@Torex.com>
2013-01-01 18:03:55 +0000

    Specify which version of Ruby for the rvm.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,6 @@
-require "bundler/capistrano"
-#require 'rvm/capistrano'
+$:.unshift(File.expand_path('./lib', ENV['rvm_path']))
+#require "bundler/capistrano"
+require 'rvm/rvm-capistrano'
 
 set :rvm_ruby_string, "1.9.3-p327"
 set :rvm_type, :user
