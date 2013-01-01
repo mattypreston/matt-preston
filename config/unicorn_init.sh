c7215f5bad39894f382d17e4010fa936690ca8ec
mattypreston <Matthew.Preston@Torex.com>
2013-01-01 18:51:37 +0000

    Tweaks to unicorn config.

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -5,10 +5,11 @@ set -e
 
 # Feel free to change any of the following variables for your app:
 TIMEOUT=${TIMEOUT-60}
-APP_ROOT=/home/ubuntu/matt-preston/current
+APP_ROOT=/home/ubuntu/apps/matt-preston/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
 CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=ubuntu
+
 set -u
 
 old_pid="$PID.oldbin"
